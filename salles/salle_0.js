c19e4e19e980870a813570bfd3bf01ca3071d7bd
DamienLeMalETPA <damienlemal123@gmail.com>
2020-03-12 13:03:24 +0100

    te

--- salles/salle_0.js
+++ salles/salle_0.js
@@ -8,11 +8,14 @@ class salle_0 extends Phaser.Scene {
 	}
 
 	preload() {
-		
+		this.load.image('back','assets/violet.jpg');
+		this.load.image('fore','assets/blue.jpg');
 	}
 
 	create() {
-		
+		this.add.image(100,100,'back').setOrigin(0,0).setScale(1.31,0.98);
+		this.add.image(1000, 1000,'fore').setOrigin(0,0).setScale(4,4);
+		//camera.setViewport
 	}
 
 	update() {
